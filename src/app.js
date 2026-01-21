93258eff73829bdfa02d9cdea345608268f334a6
Gitsharadravi <kumarsharadravi@gmail.com>
2026-01-21 18:09:58 +0530

    new fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 const app = express();
 
 //basic configurations
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 // cors configuration
 app.use(cors({
@@ -27,5 +29,19 @@ app.get('/', (req, res) => {
   res.send("welcome to basecamp");
 })
 
+// for next() error--------------------------------------------
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong";
+  
+  return res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+    errors: err.errors || [],
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined
+  });
+});
+//-----------------------------------------------------------
 
 export default app;
